264aea4cd0373bd469fd8b9142e31a54fb7e039c
dhruv0000 <patel.4@iitj.ac.in>
2021-03-22 01:36:24 +0530

    :milky_way: Updated Graphs
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- server/plot/cdf.py
+++ server/plot/cdf.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from numpy import hstack
 from statsmodels.distributions.empirical_distribution import ECDF
 
-path = 'data_tcp/50/latencyViewer*'
+path = 'data_udp/50/latencyViewer*'
 files=glob.glob(path)   
 total = 0
 n = 0
@@ -41,9 +41,10 @@ ecdf = ECDF(sample)
 pyplot.plot(ecdf.x,ecdf.y)
 pyplot.xlabel("Latency (ms)")
 pyplot.title("50 Clients")
+pyplot.xlim(0,300)
 pyplot.ylabel("Cumulative Probability")
 # pyplot.show()
-pyplot.savefig('TCP_50.png')
+pyplot.savefig('UDP_50.png')
 print(ve)
 print(n)
 print(total*1000/n)
